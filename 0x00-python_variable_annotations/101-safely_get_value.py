408047367b8886277b9de40199e802323266fa7a
Joseph Oluchukwu <devjoseph016@gmail.com>
2024-10-21 08:19:42 +0100

    Modified file for Task 11 to pass pycodestyle

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -5,18 +5,21 @@ from typing import TypeVar, Mapping, Any, Union
 
 T = TypeVar('T')
 
-def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+
+def safely_get_value(dct: Mapping,
+                     key: Any,
+                     default: Union[T, None] = None) -> Union[Any, T]:
     """
     Returns the first element of a given sequence,
     or None if the sequence is empty.
-    
+
     Parameters:
     -----------
     lst : Sequence[Any]
         A sequence (e.g., list, tuple, string) containing elements of any type.
     key: Any
     default: Union[T, None]
-    
+
     Returns:
     --------
     Union[Any, None]

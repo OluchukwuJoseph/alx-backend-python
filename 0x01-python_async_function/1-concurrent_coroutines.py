8ca69e35c561b7f586f69b5be526a2e57d782712
Joseph Oluchukwu <devjoseph016@gmail.com>
2024-10-23 07:20:26 +0100

    Added files for task 2 and 3

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,14 +2,13 @@
 """ This module contains the wait_n function """
 import asyncio
 from random import uniform
-
-
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> list:
+    """ Spawn wait_random n times with the specified max_delay """
     delays = []
     tasks = [wait_random(max_delay) for _ in range(n)]
-    print(tasks)
     for fut in asyncio.as_completed(tasks):
         result = await fut
         delays.append(result)

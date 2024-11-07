ad2fd9027cc83db3852028ae7d9240fb6783605b
Joseph Oluchukwu <devjoseph016@gmail.com>
2024-11-07 16:30:08 +0100

    Resolved issues

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """ This script contains the async_generator coroutine """
-from typing import Generator, AsyncGenerator
+from typing import Generator
 import asyncio
 import random
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """ Yield a random number between 0 and 10 """
     for _ in range(10):
         await asyncio.sleep(1)
--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -6,6 +6,7 @@ import asyncio
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
+
 async def measure_runtime() -> float:
     """
     Measure the total runtime of 4 async_comprehension
